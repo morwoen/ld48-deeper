8ee5edf75624b1ae693ea33d5b4ddab5568700df
Georgi Tsaklev <georgi.tsaklev@gmail.com>
2021-04-24 12:54:45 +0100

    Add support for lazor movement

--- Assets/Scripts/Laser.cs
+++ Assets/Scripts/Laser.cs
@@ -10,6 +10,13 @@ public class Laser : MonoBehaviour
   private Material material;
   [SerializeField]
   private GameObject beamEnd;
+  [SerializeField]
+  private float movementSpeed = 1;
+  [SerializeField]
+  private Transform[] lookAtLocations;
+
+  private Vector3 currentLookLocation;
+  private int nextLookLocation;
 
   void Start() {
     lazor = gameObject.AddComponent(typeof(LineRenderer)) as LineRenderer;
@@ -17,13 +24,25 @@ public class Laser : MonoBehaviour
     lazor.endWidth = 0.02f;
     lazor.material = material;
     beamEnd.SetActive(false);
+    currentLookLocation = transform.up;
+    nextLookLocation = 0;
   }
 
   void Update() {
-    lazor.SetPosition(0, transform.position);
+    lazor.SetPosition(0, transform.position + transform.up * 0.05f);
+
+    if (lookAtLocations.Length > 0) {
+      var currentTargetLocation = lookAtLocations[nextLookLocation];
+
+      if (Vector3.Distance(currentTargetLocation.position, currentLookLocation) < 0.01) {
+        nextLookLocation = (nextLookLocation + 1) % lookAtLocations.Length;
+      }
+
+      currentLookLocation = Vector3.MoveTowards(currentLookLocation, currentTargetLocation.position, movementSpeed * Time.deltaTime);
+    }
 
     RaycastHit hit;
-    if (Physics.Raycast(transform.position, transform.up, out hit)) {
+    if (Physics.Raycast(transform.position, currentLookLocation, out hit)) {
       if (hit.collider) {
         lazor.SetPosition(1, hit.point);
         beamEnd.transform.position = hit.point;
