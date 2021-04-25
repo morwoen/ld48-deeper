94b3db06b2558e1c5c19e57c04d296eb5b96a9cc
Dan Mc <danmckinley54@gmail.com>
2021-04-25 15:12:53 +0100

    Got movement working with FSM

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,97 +3,91 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using FSM;
 
-public class PlayerController : MonoBehaviour {
-
-    public PlayerInput input;
+public class PlayerController : MonoBehaviour
+{
     public CharacterController controller;
     public Transform cameraTransform;
+    public InputManager inputManager;
 
-    public float speed = 6f;
-    public float gravity = -9.81f;
-    public float jumpHeight = 3f;
-    public float turnSpeed = 0.1f;
-    public float turnTime = 0.1f;
+    [SerializeField] public float speed = 6f;
+    [SerializeField] public float gravity = -9.81f;
+    [SerializeField] public float jumpHeight = 3f;
+    [SerializeField] public float turnTime = 0.1f;
+    // public float turnSpeed = 0.1f;
 
     private Vector3 velocity;
     float turnSmoothVelocity;
+    private StateMachine fsm;
 
-    Vector2 moveInputDirection;
-    bool moveInput;
-    bool runInput;
-    bool jumpInput;
-    bool crouchInput;
+    public bool canMove;
 
     float originalScale;
 
-    void Awake() {
-        input = new PlayerInput();
-        input.PlayerControls.Locomotion.performed += ctx => {
-            moveInputDirection = ctx.ReadValue<Vector2>();
-            moveInputDirection = ctx.ReadValue<Vector2>();
-            moveInput = moveInputDirection.x != 0 || moveInputDirection.y != 0;
-        };
-        input.PlayerControls.Run.performed += ctx => runInput = ctx.ReadValueAsButton();
-        input.PlayerControls.Jump.performed += ctx => jumpInput = ctx.ReadValueAsButton();
-        input.PlayerControls.Crouch.performed += ctx => crouchInput = ctx.ReadValueAsButton();
-
-    }
-
     void Start()
     {
-        //animator = GetComponent<Animator>();
-        originalScale = transform.localScale.y;
+        originalScale = transform.localScale.y; // TODO: Implement proper crouching
+
+        inputManager = GetComponent<InputManager>();
+
+        fsm = new StateMachine(this);
+        fsm.AddState("Movement", new MovementState(this));
+
+        // This configures the entry point of the state machine
+        fsm.SetStartState("Movement");
+        // Initialises the state machine and must be called before OnLogic() is called
+        fsm.OnEnter();
     }
 
     void Update()
     {
-        HandleGravity();
+        fsm.OnUpdate();
         HandleJump();
         HandleMovement();
+        HandleCrouch();
     }
 
+    private void FixedUpdate()
+    {
+        HandleGravity();
+        fsm.OnFixedUpdate();
+    }
+
+    public float currentSpeed
+    {
+        get;
+        private set;
+    }
+    
     private void HandleJump()
     {
         //Debug.Log($"Jump Input: {jumpInput}");
-        if(jumpInput && controller.isGrounded)
+        if (inputManager.jumpInput && controller.isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
         }
     }
 
-    void OnEnable()
+    public void HandleMovement()
     {
-        input.PlayerControls.Enable();
-    }
-
-    void OnDisable()
-    {
-        input.PlayerControls.Disable();
-    }
-
-
-    void HandleMovement()
-    {
-        if (moveInput)
+        if (canMove && inputManager.moveInputDirection.magnitude > 0)
         {
-            // animator.SetBool(isWalking, true);
-            Vector3 direction = new Vector3(moveInputDirection.x, 0, moveInputDirection.y).normalized;
+            Debug.Log("Trying to do movement");
+            Vector3 direction = new Vector3(inputManager.moveInputDirection.x, 0, inputManager.moveInputDirection.y).normalized;
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cameraTransform.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnTime);
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
             Vector3 moveDir = (Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward).normalized;
             controller.Move(moveDir * speed * Time.deltaTime);
         }
-        HandleCrouch();
     }
 
     private void HandleCrouch()
     {
-        
-        if (crouchInput)
+        if (inputManager.crouchInput)
         {
-            transform.localScale = new Vector3(transform.localScale.x, originalScale/2, transform.localScale.z);
+            transform.localScale = new Vector3(transform.localScale.x, originalScale / 2, transform.localScale.z);
         }
         else
         {
---
+++ Assets/Scripts/States/MovementState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using FSM;
+
+public class MovementState : StateBase
+{
+  private PlayerController player;
+  private float turnSmoothVelocity;
+  private Transform cam;
+  private InputManager inputManager;
+  private Vector3 axis;
+
+  public MovementState(PlayerController player) : base(false) {
+    this.player = player;
+    cam = Camera.main.transform;
+    inputManager = player.GetComponent<InputManager>();
+    axis = Vector3.right + Vector3.forward;
+  }
+
+    public override void OnEnter()
+    {
+        player.canMove = true;
+    }
+
+    public override void OnExit()
+    {
+        player.canMove = false;
+    }
+
+    public override void OnFixedUpdate() {
+    // var movement = inputManager.moveInputDirection;
+    /* if (movement.magnitude > 0) {
+      float inputAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Rad2Deg + cam.eulerAngles.y;
+      float angle = Mathf.SmoothDampAngle(player.transform.eulerAngles.y, inputAngle, ref turnSmoothVelocity, player.turnTime);
+      player.transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+      player.SetMove(Quaternion.Euler(0.0f, inputAngle, 0.0f) * Vector3.forward * player.currentSpeed, axis);
+    }*/
+  }
+}
